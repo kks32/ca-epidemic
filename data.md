dee1b868f4f2174ef8a781d3890cd5e89f3aa9f9
Krishna Kumar <kks32@cam.ac.uk>
2017-11-02 00:10:31 +0000

    :construction: WIP different species

--- data.md
+++ data.md
@@ -18,25 +18,46 @@ Long: -17.31 + 35.875 => 53.185 ==> 64.35pixels from left Africa (71 - 75 pixels
 Lat: -36.71 + 0.753 = 37.462 ==> 44 pixels from bottom Africa (49 - 53 pixels) (edited)
 
 
-## Ardipithecus
+## Ardipithecus K
 > 10.28000000 40.25861111 (NA time line)
-* Long: -17.31 + 40.25861111 => 57.57 ==> 69.7 pixels from left Africa (77 - 81 pixels)
+* Long: -17.31 + 40.25861111 => 57.57 ==> 69.7 pixels from left Africa (68 + 9 - 72 + 9 pixels)
 * Lat: -36.71 + 10.28 = 46.99 ==> 55.26 pixels from bottom Africa (53+7 - 57+7 pixels)
 
-## Australopithecus
-> 16.065    18.50083333
+> 5.77 - 5.54 MYA (From t0 0  - 0.06 MYA)
 
-* Long: -17.31 + 18.5 => 35.81 ==> 43.33 pixels from left Africa (41 + 9 - 45 + 9 pixels)
-* Lat: -36.71 + 16.065 => 52.78 ==> 62.063 pixels from bottom Africa (60+7 - 64+7 pixels)
+* steps = 0 - 60
+
+
+## Ardipithecus R
+> 10.479 40.4375 (NA time line)
+* Long: -17.31 + 40.4375 => 57.75 ==> 69.875 pixels from left Africa (68 + 9 - 72 + 9 pixels)
+* Lat: -36.71 + 10.479 = 47.189 ==> 55.494 pixels from bottom Africa (53+7 - 57+7 pixels)
+
+> 4.45 - 4.35 MYA (From t0 1.15  - 1.25 MYA)
+
+* steps = 1150 - 1250
+
+## Australopithecus Ana 
+> 3.583    36.116
+
+* Long: -17.31 + 36.12 => 53.43 ==> 64.65 pixels from left Africa (62 + 9 - 66 + 9 pixels)
+* Lat: -36.71 + 3.58 = 40.29 ==> 47.38 pixels from bottom Africa (45+7 - 49+7 pixels)
+
+> 4.2 - 3.9 MYA (From t0 1.4  - 1.7 MYA)
+
+* steps = 1400 - 1700
+
+## bahrelghazali 3.58333333    36.11666667
+* Long: -17.31 + 36.12 => 53.43 ==> 64.65 pixels from left Africa (62 + 9 - 66 + 9 pixels)
+* Lat: -36.71 + 3.58 = 40.29 ==> 47.38 pixels from bottom Africa (45+7 - 49+7 pixels)
 
 > 3.65 - 3.55 MYA (From t0 1.95  - 2.05 MYA)
 
-* steps = 1950 - 2050
-
-##   bahrelghazali 3.58333333    36.11666667
+## Kenyanthropus 3.58333333 36.1166667
 * Long: -17.31 + 36.12 => 53.43 ==> 64.65 pixels from left Africa (62 + 9 - 66 + 9 pixels)
 * Lat: -36.71 + 3.58 = 40.29 ==> 47.38 pixels from bottom Africa (45+7 - 49+7 pixels)
 
 > 3.55 - 3.45 MYA (From t0 2.05  - 2.15 MYA)
+
 
 * steps = 2050 - 2150
--- epidemic/model.py
+++ epidemic/model.py
@@ -14,11 +14,14 @@ class Epidemic(Model):
     Represents the 2-dimensional array of cells in Conway's
     Game of Life.
     '''
-
     def __init__(self, height, width, map):
         '''
         Create a new playing area of (height, width) cells and map.
         '''
+        # global time
+        self.globaltime = 0
+
+
         # Greyscale image
         self.map = map 
          
@@ -47,19 +50,27 @@ class Epidemic(Model):
                     cell.infection = random()
                     cell.infectious = True
                     cell.mutability = cell.IMMUTABLE
+                # Ardipithecus K
+                if x > 77  and x < 81 and y > 60 and y < 64:
+                    cell.state = cell.DEAD
+                    cell.mobility = cell.MOBILE
+                    cell.infection = 0.
+                    cell.starttime = 0
+                    cell.endtime = 60
+                    cell.infectious = True
                 if x > 50  and x < 54 and y > 67 and y < 71:
                 # if y > 60 and y < 65 and x > 65 and x < 70:
                     cell.state = cell.DEAD
                     cell.mobility = cell.MOBILE
                     cell.infection = 0.
-                    cell.starttime = 0 #1950
-                    cell.endtime = 280 #2050
+                    cell.starttime =1950
+                    cell.endtime = 2050
                     cell.infectious = True
                 if x > 71  and x < 75 and y > 52 and y < 56:
                 # if y > 60 and y < 65 and x > 65 and x < 70:
                     cell.state = cell.DEAD
-                    cell.starttime = 0 #2050
-                    cell.endtime = 350 #2150
+                    cell.starttime = 2050
+                    cell.endtime = 2150
                     cell.mobility = cell.MOBILE
                     cell.infection = 0.
                     cell.infectious = True
@@ -75,3 +86,24 @@ class Epidemic(Model):
         Have the scheduler advance each cell by one step
         '''
         self.schedule.step()
+
+        # global time
+        self.globaltime += 1
+
+        if self.globaltime > 88:
+            for (contents, x, y) in self.grid.coord_iter():
+                cell = Cell((x, y), self)
+                # Ardipithecus R
+                if x > 77  and x < 81 and y > 60 and y < 64:
+                    cell.state = cell.DEAD
+                    cell.mobility = cell.MOBILE
+                    cell.infection = 0.
+                    cell.starttime =90 #1150
+                    cell.endtime = 150 #1250
+                    cell.infectious = True
+                    cell.globaltime = self.globaltime
+                    
+                    self.grid.place_agent(cell, (x, y))
+                    self.schedule.add(cell)
+
+                    print ("Hello")
